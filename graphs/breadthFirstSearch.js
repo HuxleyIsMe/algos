703c97a750e9f9516b2e838a3352164e7d657c0e
HuxleyIsMe <huxley.millard@gmail.com>
2025-04-20 17:22:06 +0100

    feat: fetched the graph stuff from the older repo

---
+++ graphs/breadthFirstSearch.js
@@ -0,0 +1,80 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+  breadthFirstSearch(start) {
+    let result = [];
+    let visited = { [start]: true };
+    let queue = [start];
+
+    while (queue.length > 0) {
+      let current = queue.shift();
+      result.push(current);
+
+      let nextNodes = this.adjacencyList[current]; // A{ BD} = [BD]
+
+      nextNodes.forEach((node) => {
+        if (!visited[node]) {
+          visited[node] = true;
+          queue.push(node);
+        }
+      });
+    }
+
+    console.log({ visited });
+
+    return result;
+  }
+}
+
+var graph = new Graph();
+
+graph.addVertex("S");
+graph.addVertex("P");
+graph.addVertex("U");
+graph.addVertex("X");
+graph.addVertex("Q");
+graph.addVertex("Y");
+graph.addVertex("V");
+graph.addVertex("R");
+graph.addVertex("W");
+graph.addVertex("T");
+
+graph.addEdge("S", "P");
+graph.addEdge("S", "U");
+
+graph.addEdge("P", "X");
+graph.addEdge("U", "X");
+
+graph.addEdge("P", "Q");
+graph.addEdge("U", "V");
+
+graph.addEdge("X", "Q");
+graph.addEdge("X", "Y");
+graph.addEdge("X", "V");
+
+graph.addEdge("Q", "R");
+graph.addEdge("Y", "R");
+
+graph.addEdge("Y", "W");
+graph.addEdge("V", "W");
+
+graph.addEdge("R", "T");
+graph.addEdge("W", "T");
+
+console.log(graph.breadthFirstSearch("S"));
---
+++ graphs/graphSearch.js
@@ -0,0 +1,79 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+  depthFirstSearch(start) {
+    let visited = { [start]: true };
+    let stack = [start];
+    let result = [];
+
+    while (stack.length > 0) {
+      let current = stack.pop();
+
+      result.push(current);
+
+      let nextNodes = this.adjacencyList[current]; // i.e A {B.D} = BD
+
+      nextNodes.forEach((node) => {
+        if (!visited[node]) {
+          visited[node] = true;
+          stack.push(node);
+        }
+      });
+    }
+
+    return result;
+  }
+}
+
+var graph = new Graph();
+
+graph.addVertex("S");
+graph.addVertex("P");
+graph.addVertex("U");
+graph.addVertex("X");
+graph.addVertex("Q");
+graph.addVertex("Y");
+graph.addVertex("V");
+graph.addVertex("R");
+graph.addVertex("W");
+graph.addVertex("T");
+
+graph.addEdge("S", "P");
+graph.addEdge("S", "U");
+
+graph.addEdge("P", "X");
+graph.addEdge("U", "X");
+
+graph.addEdge("P", "Q");
+graph.addEdge("U", "V");
+
+graph.addEdge("X", "Q");
+graph.addEdge("X", "Y");
+graph.addEdge("X", "V");
+
+graph.addEdge("Q", "R");
+graph.addEdge("Y", "R");
+
+graph.addEdge("Y", "W");
+graph.addEdge("V", "W");
+
+graph.addEdge("R", "T");
+graph.addEdge("W", "T");
+
+console.log(graph.depthFirstSearch("S"));
